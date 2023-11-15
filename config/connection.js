ccd378b0d18da84c02438e43447cb02bc45835dd
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-11-14 19:05:36 -0500

    added files and folders

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
-
+// const connectionStringURI = `mongodb://127.0.0.1:27017/NameOfDBGoesHere`;
 
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const routes = require('./routes');
 
 const mongoose = require('mongoose');
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
+
+ 
